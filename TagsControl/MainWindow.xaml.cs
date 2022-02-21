74601e04de1fbdf58f716f31f2c29be31d1b2bdd
AYarkevich <evrosrv@gmail.com>
2022-02-21 22:38:46 +0300

    Some commit

--- TagsControl/MainWindow.xaml.cs
+++ TagsControl/MainWindow.xaml.cs
@@ -1,13 +1,21 @@
-﻿namespace TagsControl
+﻿using System.Collections.Generic;
+
+namespace TagsControl
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            Items = new List<string>()
+            {
+                "first item",
+                "second item",
+                "third item"
+            };
         }
+
+        public List<string> Items { get; set; }
     }
 }
---
+++ TagsControl/TagControl/TagControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TagsControl.TagControl
+{
+    public class TagControl : Control
+    {
+        public static readonly DependencyProperty TagsItemsProperty =
+            DependencyProperty.Register(nameof(TagsItems), typeof(IEnumerable<string>), typeof(TagControl),
+                new PropertyMetadata(default(IEnumerable<string>)));
+
+        public IEnumerable<string> TagsItems
+        {
+            get => (IEnumerable<string>) GetValue(TagsItemsProperty);
+            set => SetValue(TagsItemsProperty, value);
+        }
+    }
+}
